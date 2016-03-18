7a363facaf0c2d3da4e486935a1670967e2db8b4
snbrown07 <sbrow5081@withs.org>
2016-03-18 13:56:35 +0000

    jQuery Funhouse

---
+++ solution.js
@@ -0,0 +1,23 @@
+function greetUser(name){
+    $("#greeting").text("Hello, "+ name);
+}
+function switchImage(picture){
+    $("#gallery-main").attr("src", picture);
+}
+
+
+
+ var safeColors = ['00', '33', '66', '99', 'cc', 'ff'];
+ var rand= function(){
+     return Math.floor(Math. random()*6);
+ };
+ var randomColor = function (){
+     var r= safeColors[rand()];
+     var g= safeColors[rand()];
+     var b= safeColors[rand()];
+     return "#"+r+g+b;
+ };
+ 
+ function changeBackgroundColor(){
+     $("body").css("background-color", randomColor);
+ } 
